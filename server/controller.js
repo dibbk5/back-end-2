8f7f352d38d9a955f67951ed050f603c0e88e72d
Koy Dibb <dibbk5@gmail.com>
2023-01-05 20:34:37 -0700

    Prettier

--- server/controller.js
+++ server/controller.js
@@ -1,48 +1,47 @@
-let houses = require('./db.json')
-let houseID = 4
+let houses = require("./db.json");
+let houseID = 4;
 
 module.exports = {
-    getHouses:(req,res) => {
-        res.status(200).send(houses)
-    },
-    deleteHouse(req,res) {
-        let indexHouse = houses.findIndex(home => home.id === +req.params.id)
-        houses.splice(indexHouse, 1)
+  getHouses: (req, res) => {
+    res.status(200).send(houses);
+  },
+  deleteHouse(req, res) {
+    let indexHouse = houses.findIndex((home) => home.id === +req.params.id);
+    houses.splice(indexHouse, 1);
 
-        res.status(200).send(houses)
-    },
-    createHouse(req,res) {
-        let { address, price, imageURL } = req.body;
+    res.status(200).send(houses);
+  },
+  createHouse(req, res) {
+    let { address, price, imageURL } = req.body;
 
-        if (!address || !price || !imageURL) {
-            return res.status(400).send("Invalid request.")
-        }
-
-        const newHouse = {
-            id: houseID,
-            address,
-            price,
-            imageURL
-        }
-
-        houses.push(newHouse)
-        houseID += 1
-        res.status(200).send(houses)
-    },
-    updateHouse(req,res) {
-        let { type } = req.body
-        let indexHouse = houses.findIndex(home => home.id === +req.params.id)
-
-        if (type === 'minus' && houses[indexHouse].price <= 10000) {
-            houses[indexHouse].price = 0
-            res.status(200).send(houses)
-        } else if (type === 'minus') {
-            houses[indexHouse].price -= 10000
-            res.status(200).send(houses)
-        } else if (type === 'plus') {
-            houses[indexHouse].price += 10000
-            res.status(200).send(houses)
-        }
+    if (!address || !price || !imageURL) {
+      return res.status(400).send("Invalid request.");
     }
 
-}
+    const newHouse = {
+      id: houseID,
+      address,
+      price,
+      imageURL,
+    };
+
+    houses.push(newHouse);
+    houseID += 1;
+    res.status(200).send(houses);
+  },
+  updateHouse(req, res) {
+    let { type } = req.body;
+    let indexHouse = houses.findIndex((home) => home.id === +req.params.id);
+
+    if (type === "minus" && houses[indexHouse].price <= 10000) {
+      houses[indexHouse].price = 0;
+      res.status(200).send(houses);
+    } else if (type === "minus") {
+      houses[indexHouse].price -= 10000;
+      res.status(200).send(houses);
+    } else if (type === "plus") {
+      houses[indexHouse].price += 10000;
+      res.status(200).send(houses);
+    }
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const houseController = require('./controller')
+const houseController = require("./controller");
 
 const PORT = 4004;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/houses', houseController.getHouses);
-app.post('/api/houses', houseController.createHouse);
-app.put('/api/houses/:id', houseController.updateHouse);
-app.delete('/api/houses/:id', houseController.deleteHouse);
-
+app.get("/api/houses", houseController.getHouses);
+app.post("/api/houses", houseController.createHouse);
+app.put("/api/houses/:id", houseController.updateHouse);
+app.delete("/api/houses/:id", houseController.deleteHouse);
 
 app.listen(4004, () => console.log(`Server running on ${PORT}`));
